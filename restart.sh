e9db06365e183801b262c9833dcfe538cdaccfc9
asaied07 <anassid07@gmail.com>
2025-02-27 13:27:16 -0500

    agregando pagina inicial y algo de estructura

---
+++ restart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker compose down
+rm -rf dbdata/
+docker system prune -a -f
+docker compose up -d
--- scientifiks/app.py
+++ scientifiks/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app=Flask(__name__)
 
 @app.route('/')
-
-def home():
-    return '<h1> Hola Mundo </h1>'
+def index():
+    # aqui retorna lo que uno le diga (puede ser un texto simple o una pagina web)
+    # para este caso vamos a redirigil a index.html
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='9500')
