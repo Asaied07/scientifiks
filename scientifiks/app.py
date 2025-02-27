593442559ee71017343e935390c0ccc74ece280d
asaied07 <anassid07@gmail.com>
2025-02-27 13:59:41 -0500

    agregando una pagina acerca de para mostrar tecnologias usadas

--- scientifiks/app.py
+++ scientifiks/app.py
@@ -8,5 +8,11 @@ def index():
     # para este caso vamos a redirigil a index.html
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    # aqui retorna lo que uno le diga (puede ser un texto simple o una pagina web)
+    # para este caso vamos a redirigil a about.html
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='9500')
