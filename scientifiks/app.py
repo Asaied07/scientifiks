4b8960cc985df09db7edaa04a2b4dd0548c6ac71
asaied07 <anassid07@gmail.com>
2025-03-17 19:10:42 -0500

    Agregando producido

--- scientifiks/app.py
+++ scientifiks/app.py
@@ -14,5 +14,11 @@ def about():
     # para este caso vamos a redirigil a about.html
     return render_template('about.html')
 
+@app.route('/producido')
+def producido():
+    # aqui retorna lo que uno le diga (puede ser un texto simple o una pagina web)
+    # para este caso vamos a redirigil a about.html
+    return render_template('producido.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='9500')
